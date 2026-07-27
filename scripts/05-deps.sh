f3fbe9a103bfe6f3050e28df70fcdaeda48344b5
jiaqingxu888 <564034054@qq.com>
2026-07-27 20:40:01 +0800

    Create 05-deps.sh

---
+++ scripts/05-deps.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -e
+sudo apt-get update
+sudo apt-get install -y build-essential clang flex bison g++ gawk gcc-multilib gettext git libncurses5-dev libssl-dev python3-distutils python3-setuptools rsync swig unzip zlib1g-dev file wget ca-certificates python3 python3-pip libelf-dev ecj fastjar
